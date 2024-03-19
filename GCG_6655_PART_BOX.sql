e3702dce52a375d0b1a02714a8ed3be77efe5b5c
Service Web <sw@gssmail.com>
2024-03-19 11:02:36 -0500

    Commit changes in C:\Websites\Git\emarlow\6655\GCG_6655_PART_BOX.sql

---
+++ GCG_6655_PART_BOX.sql
@@ -0,0 +1,14 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Jul 14, 2022
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6655_PART_BOX" (
+ "Part" CHAR(20),
+ "BoxNum" INTEGER,
+ "NumPcs" INTEGER,
+ "PO_Number" CHAR(7),
+ "PO_Line" CHAR(4),
+ "Sqr_Ft" DOUBLE );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
