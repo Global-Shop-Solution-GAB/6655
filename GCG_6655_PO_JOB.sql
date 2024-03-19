391edf89df95242beeb03e9dc917c697df92203b
Service Web <sw@gssmail.com>
2024-03-19 11:02:36 -0500

    Commit changes in C:\Websites\Git\emarlow\6655\GCG_6655_PO_JOB.sql

---
+++ GCG_6655_PO_JOB.sql
@@ -0,0 +1,12 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Jul 14, 2022
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6655_PO_JOB" (
+ "JOB" CHAR(6),
+ "SUFFIX" CHAR(3),
+ "PO_NUMBER" CHAR(7),
+ "PO_LINE" CHAR(4) );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
